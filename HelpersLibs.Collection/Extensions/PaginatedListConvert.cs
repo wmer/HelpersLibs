f299a7ed40294206d33da11fba105231928d40bf
William Santana 4C Digital <william.santana@4cdigital.com.br>
2026-02-06 00:07:56 -0300

    Configurado LogCOntroller e configurações de segurança

---
+++ HelpersLibs.Collection/Extensions/PaginatedListConvert.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpersLibs.Collection.Extensions; 
+public static class PaginatedListConvert {
+    public static PaginatedList<U> Convert<T, U>(this PaginatedList<T> source, Func<PaginatedList<T>, PaginatedList<U>> paginatedList) {
+        return paginatedList.Invoke(source);
+    }
+}
